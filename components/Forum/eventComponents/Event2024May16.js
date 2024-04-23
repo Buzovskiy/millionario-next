22e10ae9eb4fd8b88b18cf3590319db7fa465d1c
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2024-04-23 20:11:07 +0200

    add forum

--- components/Forum/eventComponents/Event2024May16.js
+++ components/Forum/eventComponents/Event2024May16.js
@@ -3,6 +3,7 @@ import React, {useEffect} from "react";
 import {initTicketForEvent} from "../ticketforevent";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../Forum.module.css";
 
 
 const ButtonForEvent = (props) => {
@@ -17,12 +18,13 @@ const ButtonForEvent = (props) => {
       )
    } else {
       return (
-         <a className="mil-btn epts-cb-green2 tfe-embed-popup"
+         <a className="epts-cb-green2 tfe-embed-popup btn-silver"
             data-alias="millionario-alicante-16-04-24"
             data-lang="ru"
             target="_blank"
+            style={props.style}
             href="https://millionario-alicante-16-04-24.ticketforevent.com/ru/?noredirect">
-            Выбрать билеты
+            Купить билет
          </a>
       )
    }
@@ -37,16 +39,171 @@ const Event2024May16 = () => {
 
    return (
       <>
-         <div className="container forum event-2 event-2024-02-09">
-            <div className="row py-5">
-               <div className="forum-invite-wrapper">
-                  <img src="/img/forum/event-2024-05-16/img.png" alt=""/>
+         <div className="container-fluid">
+            <div className={`row ${styles.desktop}`}>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice1.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
                </div>
-               <div className="forum-invite-wrapper" style={{height: '300px'}}>
-                  <ButtonForEvent/>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice2_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice3.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice4_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice5.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice6_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice7.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/slice8_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{}}/>
+               </div>
+            </div>
+
+            <div className={`row ${styles.mobile}`}>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/mob/slice1mob.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/mob/slice2mob_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/mob/slice3mob_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{bottom: '50px'}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/mob/slice4mob_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{bottom: '100px'}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/mob/slice5mob_btn.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
+                  <ButtonForEvent style={{bottom: '100px'}}/>
+               </div>
+               <div className={styles.block}>
+                  <Image
+                     src="/img/forum/event-2024-05-16/mob/slice6mob.png"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     style={{width: '100%', height: 'auto'}}
+                     alt="forum"
+                  />
                </div>
             </div>
          </div>
+         {/*<div className="container forum event-2 event-2024-02-09">*/}
+         {/*   <div className="row py-5">*/}
+         {/*      <div className="forum-invite-wrapper">*/}
+         {/*         <img src="/img/forum/event-2024-05-16/img.png" alt=""/>*/}
+         {/*      </div>*/}
+         {/*      <div className="forum-invite-wrapper" style={{height: '300px'}}>*/}
+         {/*         <ButtonForEvent/>*/}
+         {/*      </div>*/}
+         {/*   </div>*/}
+         {/*</div>*/}
       </>
    );
 };
