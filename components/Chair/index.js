2cff11a20b84ac4ad18f828c0918156c34596748
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2024-05-24 20:01:29 +0200

    change images

--- components/Chair/index.js
+++ components/Chair/index.js
@@ -3,14 +3,15 @@ import './style.scss';
 
 const Chair = () => {
    return (
-      <div className="container chair">
+      <div className="container-fluid chair">
          <div className="row py-5">
             <div className="row-wrapper">
                <div className="section-heading">
                   а ТИ ВЖЕ СЕРЕД УСПIШНИХ?
                </div>
                <div className="chair__wrapper">
-                  <img src="/img/chair.png" alt=""/>
+                  <img className="desktop" src="/img/chair.png" alt=""/>
+                  <img className="mobile" src="/img/chair-mobile.png" alt=""/>
                   <Link href="/apply-for-short" className="mil-btn">подати заявку</Link>
                </div>
             </div>
